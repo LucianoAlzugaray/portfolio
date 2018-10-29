a681f0bf8b70ef65925dfdaeec7827785f907dfd
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2018-10-29 08:40:33 -0300

    Organización un poco trabajada y topmenu agregado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SceneComponent } from './scene/scene.component';
-import { HomeComponent } from './home/home.component';
+import { SceneComponent } from './layout/scene/scene.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
+import { TopmenuComponent } from './layout/topmenu/topmenu.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SceneComponent,
+    TopmenuComponent,
     HomeComponent
   ],
   imports: [
